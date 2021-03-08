df54d1c16d08e0fad4dea2106eb70ff46cf8469f
kkris <kkris@DESKTOP-DDNH5KI>
2021-03-08 17:39:21 +0100

    Tachograph

---
+++ tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
@@ -0,0 +1,19 @@
+package hu.bme.mit.train.tachograph;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+import java.time.LocalDateTime;
+
+public class Tachograph {
+
+    private Table<LocalDateTime, Integer, Integer> tacho
+            = HashBasedTable.create();
+
+    public void add (int position, int speed) {
+        tacho.put(LocalDateTime.now(), position, speed);
+    }
+
+}
+
+
